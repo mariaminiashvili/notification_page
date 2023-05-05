c882669df6559c8ede2c1b02f7962a6dcc9d4bde
mariaminiashvili <mari.iniashvili@gmail.com>
2023-05-06 00:37:44 +0400

    notififadion page

---
+++ script.js
@@ -0,0 +1,14 @@
+const nameBox = document.querySelector(".name");
+const hiddenMessage = document.querySelector(".hidden-message");
+
+
+
+nameBox.addEventListener ("click",() =>{
+    if(click)
+    hiddenMessage.classList.displey ="flex"
+}
+)
+
+
+
+
